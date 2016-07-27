becc282cb79fdbcf5070ba8bb0fa7298ee0a6b70
yanmofeixi <yanmofeixi@gmail.com>
2016-07-26 20:34:30 -0700

    Add retry function when webjob crash

--- PokemonGoScanner/PokemonGoScanner/Models/UserSetting.cs
+++ PokemonGoScanner/PokemonGoScanner/Models/UserSetting.cs
@@ -1,6 +1,9 @@
 ﻿namespace PokemonGoScanner.Models
 {
+    using Common;
     using System.Collections.Generic;
+
+
     public class UserSetting
     {
         public string UserName;
@@ -17,5 +20,44 @@
         public string EmailToReceiveAlert;
 
         public List<PokemonId> PokemonsToIgnore;
+
+        public static List<UserSetting> InitializeUsers()
+        {
+            return new List<UserSetting>
+            {
+                new UserSetting
+                {
+                    UserName = "Qiyang Lu-Home",
+                    Email = "pkmscanner1@gmail.com",
+                    Password = "pokemongo",
+                    Latitude = 47.651168,
+                    Longitude = -122.130718,
+                    EmailToReceiveAlert = "yanmofeixi@gmail.com",
+                    PokemonsToIgnore = Constant.DefaultIgnoreList
+                },
+
+                new UserSetting
+                {
+                    UserName = "Redwest-Office",
+                    Email = "pkmscanner2@gmail.com",
+                    Password = "pokemongo",
+                    Latitude = 47.659265,
+                    Longitude = -122.140394,
+                    EmailToReceiveAlert = "yanmofeixi@gmail.com,317772270@qq.com",
+                    PokemonsToIgnore = Constant.DefaultIgnoreList
+                },
+
+                new UserSetting
+                {
+                    UserName = "Junwei Hu-Home",
+                    Email = "duxiaoccnn@gmail.com",
+                    Password = "%TGB6yhn^YHN5tgb",
+                    Latitude = 47.678306,
+                    Longitude = -122.130660,
+                    EmailToReceiveAlert = "317772270@qq.com",
+                    PokemonsToIgnore = Constant.DefaultIgnoreList
+                },
+            };
+        }
     }
 }
